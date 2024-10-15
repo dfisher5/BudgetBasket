a032738d420725c62505f62ff97ea0de145789cb
Delaney Fisher <dfisher5@uvm.edu>
2024-10-14 23:15:37 -0400

    Firebase intial set up

--- BudgetBasket/BudgetBasketApp.swift
+++ BudgetBasket/BudgetBasketApp.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct BudgetBasketApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            NavigationView() {
+                ContentView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            }
         }
     }
 }
---
+++ BudgetBasket/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  BudgetBasket
+//
+//  Created by Delaney Fisher on 10/14/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
