7294a8bc9748612cdbc8c210b026f897114a31c4
Corey Stark <cmstark@uvm.edu>
2024-11-07 11:00:36 -0500

    added firebase stuff

--- BudgetBasket/BudgetBasketApp.swift
+++ BudgetBasket/BudgetBasketApp.swift
@@ -6,31 +6,32 @@
 //
 
 import SwiftUI
-//import FirebaseCore
+import FirebaseCore
 
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(_ application: UIApplication,
-//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//    FirebaseApp.configure()
-//
-//    return true
-//  }
-//}
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct BudgetBasketApp: App {
-    let persistenceController = PersistenceController.shared
+//    let persistenceController = PersistenceController.shared
     
 //    init() {
 //        // Use Firebase library to configure APIs
 //        FirebaseApp.configure()
 //    }
-
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             NavigationView() {
-                ContentView()
-                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                HomeScreenView()
             }
         }
     }
